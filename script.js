973731b91abc745f5bec0f0000e748fd43098791
HarlandLohora <harland@lohora.com>
2021-05-18 23:56:12 -0300

    clase03 completa

--- script.js
+++ script.js
@@ -3,7 +3,22 @@ const btn = document.querySelector('[data-form-btn]');
 const createTask = (evento) => {
   evento.preventDefault();
   const input = document.querySelector('[data-form-input]');
-  console.log(input.value);
+  const value = input.value;
+  const list = document.querySelector('[data-list]');
+  const task = document.createElement('li');
+  task.classList.add('card');
+  input.value = '';
+  //backticks
+  const content = `<div>
+      <i class="far fa-check-square icon"></i>
+      <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+  task.innerHTML = content;
+
+  list.appendChild(task);
+
+  console.log(content);
 };
 
 console.log(btn);
