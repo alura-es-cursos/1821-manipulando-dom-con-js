3d3df60bf04d95198272afef5aab05cfc19ded1a
German Añorve <german.anorve@hotmail.com>
2022-11-17 16:25:22 -0600

    Agregar tareas y desplegado

--- script.js
+++ script.js
@@ -3,8 +3,21 @@ const btn=document.querySelector("[data-form-btn]");
 const createTask=  (evento) =>{
     evento.preventDefault();
     const input=document.querySelector("[data-form-input]");
-    console.log(input.value);
-    console.log(evento);
+    const list=document.querySelector("[data-list]");
+    const value=input.value;
+    input.value='';
+
+    const task=document.createElement("li");
+    task.classList.add("card");
+    const contenido=`
+    <div>
+      <i class="far fa-check-square icon"></i>
+      <span class="task">${value}</span>
+      <i class="fas fa-trash-alt trashIcon icon"></i>
+    </div>`;
+    task.innerHTML=contenido;
+    list.appendChild(task);
+    //console.log(evento);
 }
 
 btn.addEventListener("click", createTask);
