a8ff2ba581eafd0d563ac62023dc4a9529873a3e
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-07-18 14:50:59 -0600

    al boton le agregue un evento click

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-import checkComplete from './components/checkComplete.js';
-import deleteIcon from './components/deleteIcon.js';
+const btn = document.querySelector('[data-form-btn]'); // Selecciona el primer elemento del documento que tenga el atributo 'data-form-btn'.
+console.log(btn); // Imprime en la consola el elemento 'btn' seleccionado.
 
-const btn = document.querySelector('[data-form-btn]');
 
-const createTask = (evento) => {
-  evento.preventDefault();
-  const input = document.querySelector('[data-form-input]');
-  const value = input.value;
-  const list = document.querySelector('[data-list]');
-  const task = document.createElement('li');
-  task.classList.add('card');
-  input.value = '';
-  //backticks
-  const taskContent = document.createElement('div');
+btn.addEventListener('click', (evento) => { // Agrega un evento 'click' al botón seleccionado.
+  evento.preventDefault(); // Previene el comportamiento predeterminado del evento 'click', que es recargar la página en este caso.
+  const input = document.querySelector('[data-form-input]'); // Selecciona el primer elemento del documento que tenga el atributo 'data-form-input'.
+  console.log(input.value); // Imprime en la consola el valor del elemento 'input' seleccionado.
+});
 
-  const titleTask = document.createElement('span');
-  titleTask.classList.add('task');
-  titleTask.innerText = value;
-  taskContent.appendChild(checkComplete());
-  taskContent.appendChild(titleTask);
-  // task.innerHTML = content;
 
-  task.appendChild(taskContent);
-  task.appendChild(deleteIcon());
-  list.appendChild(task);
-};
+// import checkComplete from './components/checkComplete.js';
+// import deleteIcon from './components/deleteIcon.js';
 
-//Arrow functions o funciones anonimas
-btn.addEventListener('click', createTask);
+// const btn = document.querySelector('[data-form-btn]');
+
+// const createTask = (evento) => {
+//   evento.preventDefault();
+//   const input = document.querySelector('[data-form-input]');
+//   const value = input.value;
+//   const list = document.querySelector('[data-list]');
+//   const task = document.createElement('li');
+//   task.classList.add('card');
+//   input.value = '';
+//   //backticks
+//   const taskContent = document.createElement('div');
+
+//   const titleTask = document.createElement('span');
+//   titleTask.classList.add('task');
+//   titleTask.innerText = value;
+//   taskContent.appendChild(checkComplete());
+//   taskContent.appendChild(titleTask);
+//   // task.innerHTML = content;
+
+//   task.appendChild(taskContent);
+//   task.appendChild(deleteIcon());
+//   list.appendChild(task);
+// };
+
+// //Arrow functions o funciones anonimas
+// btn.addEventListener('click', createTask);
